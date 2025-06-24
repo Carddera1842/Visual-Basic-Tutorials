3c5045fa0dc52fead8f2db354ab9289a6c5e4ffc
Carddera1842 <carddera1842@gmail.com>
2025-06-24 11:31:19 -0500

    Variable Data Types

--- OutputAndVariables/Form1.vb
+++ OutputAndVariables/Form1.vb
@@ -9,7 +9,6 @@
         MsgBox("Welcome")
 
     End Sub
-
     Private Sub btnVariables_Click(sender As Object, e As EventArgs) Handles btnVariables.Click
 
         Dim stFirstName As String
@@ -26,4 +25,34 @@
         MsgBox("Hello and welcome " & stFirstName & " " & stLastName & ". I hope you are well.")
 
     End Sub
+    Private Sub btnDataTypes_Click(sender As Object, e As EventArgs) Handles btnDataTypes.Click
+
+        Dim stMake As String
+        Dim stModel As String
+        Dim iDoors As Integer
+        Dim stColor As String
+        Dim bTaxed As Boolean
+        Dim iEngineSize As Integer
+        Dim decPrice As Decimal
+        Dim dtDateRegistered As Date
+
+        stMake = "Ford"
+        stModel = "Escort"
+        iDoors = 5
+        stColor = "red"
+        bTaxed = True
+        iEngineSize = 1200
+        decPrice = 999.99
+        dtDateRegistered = #11/2/2020#
+
+        MsgBox("The car is a " & stMake & " " & stModel _
+               & vbNewLine & iDoors & " doors" _
+               & vbNewLine & stColor & " paint" _
+               & vbNewLine & bTaxed _
+               & vbNewLine & iEngineSize & " cc" _
+               & vbNewLine & decPrice _
+               & vbNewLine & dtDateRegistered)
+
+    End Sub
+
 End Class
