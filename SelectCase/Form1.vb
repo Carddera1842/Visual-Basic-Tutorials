efdba3337ab771a0c05e0d1b461e8d28d6e3027d
Carddera1842 <carddera1842@gmail.com>
2025-06-25 10:17:32 -0500

    Select Case

---
+++ SelectCase/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Private Sub btnCheck_Click(sender As Object, e As EventArgs) Handles btnCheck.Click
+
+        Dim iTemperature As Integer
+        iTemperature = CInt(txtTemperature.Text)
+
+        Dim iWindSpeed As Integer
+        iWindSpeed = 30
+
+        'Can only test one variable at a time
+        Select Case iTemperature
+            Case Is < 0
+                MsgBox("Sub Zero")
+                If iTemperature <= 0 And iWindSpeed > 20 Then
+                    MsgBox("That will feel realy cold!")
+                End If
+            Case Is = 0
+                MsgBox("Freezing")
+                MsgBox("Water will freeze")
+                MsgBox(" and you can go skating")
+            Case 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+                MsgBox("Cold")
+            Case 11 To 20
+                MsgBox("Warm")
+            Case Else
+                MsgBox("Hot")
+        End Select
+
+    End Sub
+End Class
