55a7a86dd6ab534584e66364fe0a8920ff470be4
Carddera1842 <carddera1842@gmail.com>
2025-06-25 07:53:20 -0500

    Easy Form Input

---
+++ CollectUserInput/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+
+        Dim stFristName As String
+
+        stFristName = InputBox("Please enter your first name")
+
+        MsgBox("Hello " & stFristName)
+
+    End Sub
+End Class
