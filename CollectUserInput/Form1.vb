fcee3d298cec604a1dcd68d036a30f177d526b4b
Carddera1842 <carddera1842@gmail.com>
2025-06-25 08:20:09 -0500

    Input with ListBox

--- CollectUserInput/Form1.vb
+++ CollectUserInput/Form1.vb
@@ -4,13 +4,22 @@
         Dim stFirstName As String
         Dim stLastName As String
         Dim stGender As String
+        Dim stOccupation As String
 
         stFirstName = txtFirstName.Text
         stLastName = txtLastName.Text
         stGender = txtGender.Text
+        stOccupation = lstOccupation.SelectedItem
 
-        MsgBox("Hello " & stFirstName & " " & stLastName & "! You are a " & stGender & ".")
+        MsgBox("Hello " & stFirstName & " " & stLastName & "! You are a " & stGender & " " & stOccupation & ".")
 
     End Sub
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        lstOccupation.Items.Add("Writer")
+        lstOccupation.Items.Add("Actor")
+        lstOccupation.Items.Add("Painter")
+
+    End Sub
 End Class
