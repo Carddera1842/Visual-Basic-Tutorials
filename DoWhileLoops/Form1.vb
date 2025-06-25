1b8e41205e31a74baa2291011a584a1699e2faf0
Carddera1842 <carddera1842@gmail.com>
2025-06-25 10:57:55 -0500

    Do While

---
+++ DoWhileLoops/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+
+        Dim iCount As Integer
+        iCount = 0
+
+        'More Flexible
+        Do While iCount < 5
+
+            iCount = iCount + 1
+            MsgBox("Hello Do While " & iCount)
+
+        Loop
+
+        iCount = 0
+
+        Do
+
+            iCount = iCount + 1
+            MsgBox("Hello Loop While " & iCount)
+
+        Loop While iCount < 5
+
+        iCount = 0
+
+        Do Until iCount = 5
+
+            iCount = iCount + 1
+            MsgBox("Hello Do Until " & iCount)
+
+        Loop
+
+        iCount = 0
+
+        Do
+
+            iCount = iCount + 1
+            MsgBox("Hello Loop Until " & iCount)
+
+        Loop Until iCount = 5
+
+        MsgBox("All done")
+
+    End Sub
+
+End Class
---
+++ DoWhileLoops/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("DoWhileLoops")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("DoWhileLoops")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("14322387-6c78-47f7-bd82-56d6e7f2603d")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
