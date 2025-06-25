0d4dcf66d11fb8950bd9cfeb219b31f1b0e41c20
Carddera1842 <carddera1842@gmail.com>
2025-06-25 11:24:54 -0500

    Array Variables

---
+++ ArrayVaribles/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+
+        Dim astFruits(4) As String
+
+        astFruits(0) = "Banana"
+        astFruits(1) = "Orange"
+        astFruits(2) = "Pineapple"
+        astFruits(3) = "Strawberry"
+        astFruits(4) = "Mango"
+
+        Dim i As Integer
+
+        For i = 0 To 4
+            MsgBox(astFruits(i))
+        Next
+
+    End Sub
+
+End Class
