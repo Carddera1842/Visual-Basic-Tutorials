bada137e1681d297ebd79e2af5b598f6aa7e43a4
Carddera1842 <carddera1842@gmail.com>
2025-06-26 09:56:32 -0500

    2D Arrays & Nested Loops

--- Two Dimentional Arrays/Form1.Designer.vb
+++ Two Dimentional Arrays/Form1.Designer.vb
@@ -22,29 +22,65 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.btnStart = New System.Windows.Forms.Button()
+        Me.btnRow = New System.Windows.Forms.Button()
+        Me.btnColumn = New System.Windows.Forms.Button()
+        Me.btnAll = New System.Windows.Forms.Button()
+        Me.btnSearch = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'btnStart
+        'btnRow
         '
-        Me.btnStart.Location = New System.Drawing.Point(294, 89)
-        Me.btnStart.Name = "btnStart"
-        Me.btnStart.Size = New System.Drawing.Size(285, 253)
-        Me.btnStart.TabIndex = 0
-        Me.btnStart.Text = "2D Array"
-        Me.btnStart.UseVisualStyleBackColor = True
+        Me.btnRow.Location = New System.Drawing.Point(75, 34)
+        Me.btnRow.Name = "btnRow"
+        Me.btnRow.Size = New System.Drawing.Size(603, 209)
+        Me.btnRow.TabIndex = 0
+        Me.btnRow.Text = "Display data items row wise"
+        Me.btnRow.UseVisualStyleBackColor = True
+        '
+        'btnColumn
+        '
+        Me.btnColumn.Location = New System.Drawing.Point(75, 275)
+        Me.btnColumn.Name = "btnColumn"
+        Me.btnColumn.Size = New System.Drawing.Size(603, 203)
+        Me.btnColumn.TabIndex = 1
+        Me.btnColumn.Text = "Display data items column wise"
+        Me.btnColumn.UseVisualStyleBackColor = True
+        '
+        'btnAll
+        '
+        Me.btnAll.Location = New System.Drawing.Point(75, 506)
+        Me.btnAll.Name = "btnAll"
+        Me.btnAll.Size = New System.Drawing.Size(603, 193)
+        Me.btnAll.TabIndex = 2
+        Me.btnAll.Text = "Display all data items at once"
+        Me.btnAll.UseVisualStyleBackColor = True
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(75, 744)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(603, 193)
+        Me.btnSearch.TabIndex = 3
+        Me.btnSearch.Text = "Search for a person"
+        Me.btnSearch.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(16.0!, 31.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.btnStart)
+        Me.ClientSize = New System.Drawing.Size(1232, 1066)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.btnAll)
+        Me.Controls.Add(Me.btnColumn)
+        Me.Controls.Add(Me.btnRow)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents btnStart As Button
+    Friend WithEvents btnRow As Button
+    Friend WithEvents btnColumn As Button
+    Friend WithEvents btnAll As Button
+    Friend WithEvents btnSearch As Button
 End Class
--- Two Dimentional Arrays/Form1.vb
+++ Two Dimentional Arrays/Form1.vb
@@ -1,5 +1,56 @@
 ﻿Public Class Form1
-    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnRow.Click
+
+        Dim astPeople(4, 5) As String
+
+        astPeople(0, 0) = "Barrack"
+        astPeople(1, 0) = "Obama"
+        astPeople(2, 0) = "Male"
+        astPeople(3, 0) = "American"
+        astPeople(4, 0) = "President"
+
+        astPeople(0, 1) = "Jacinda"
+        astPeople(1, 1) = "Ardern"
+        astPeople(2, 1) = "Female"
+        astPeople(3, 1) = "New Zealand"
+        astPeople(4, 1) = "Prime Minister"
+
+        astPeople(0, 2) = "Ada"
+        astPeople(1, 2) = "Lovelace"
+        astPeople(2, 2) = "Female"
+        astPeople(3, 2) = "British"
+        astPeople(4, 2) = "Mathematician"
+
+        astPeople(0, 3) = "Albert"
+        astPeople(1, 3) = "Einstein"
+        astPeople(2, 3) = "Male"
+        astPeople(3, 3) = "Swiss"
+        astPeople(4, 3) = "Scientist"
+
+        astPeople(0, 4) = "Mahatama"
+        astPeople(1, 4) = "Gandhi"
+        astPeople(2, 4) = "Male"
+        astPeople(3, 4) = "Indian"
+        astPeople(4, 4) = "Lawyer"
+
+        astPeople(0, 5) = "Vincent"
+        astPeople(1, 5) = "van Gogh"
+        astPeople(2, 5) = "Male"
+        astPeople(3, 5) = "Dutch"
+        astPeople(4, 5) = "Artist"
+
+        Dim x As Integer
+        Dim y As Integer
+
+        For y = 0 To 5
+            For x = 0 To 4
+                MsgBox(astPeople(x, y))
+            Next
+        Next
+
+    End Sub
+
+    Private Sub btnColumn_Click(sender As Object, e As EventArgs) Handles btnColumn.Click
 
         Dim astPeople(4, 5) As String
 
@@ -43,9 +94,133 @@
         Dim y As Integer
 
         For x = 0 To 4
-            MsgBox(astPeople(x, 0))
+            For y = 0 To 5
+                MsgBox(astPeople(x, y))
+            Next
         Next
 
     End Sub
 
+    Private Sub btnAll_Click(sender As Object, e As EventArgs) Handles btnAll.Click
+
+        Dim astPeople(4, 5) As String
+
+        astPeople(0, 0) = "Barrack"
+        astPeople(1, 0) = "Obama"
+        astPeople(2, 0) = "Male"
+        astPeople(3, 0) = "American"
+        astPeople(4, 0) = "President"
+
+        astPeople(0, 1) = "Jacinda"
+        astPeople(1, 1) = "Ardern"
+        astPeople(2, 1) = "Female"
+        astPeople(3, 1) = "New Zealand"
+        astPeople(4, 1) = "Prime Minister"
+
+        astPeople(0, 2) = "Ada"
+        astPeople(1, 2) = "Lovelace"
+        astPeople(2, 2) = "Female"
+        astPeople(3, 2) = "British"
+        astPeople(4, 2) = "Mathematician"
+
+        astPeople(0, 3) = "Albert"
+        astPeople(1, 3) = "Einstein"
+        astPeople(2, 3) = "Male"
+        astPeople(3, 3) = "Swiss"
+        astPeople(4, 3) = "Scientist"
+
+        astPeople(0, 4) = "Mahatama"
+        astPeople(1, 4) = "Gandhi"
+        astPeople(2, 4) = "Male"
+        astPeople(3, 4) = "Indian"
+        astPeople(4, 4) = "Lawyer"
+
+        astPeople(0, 5) = "Vincent"
+        astPeople(1, 5) = "van Gogh"
+        astPeople(2, 5) = "Male"
+        astPeople(3, 5) = "Dutch"
+        astPeople(4, 5) = "Artist"
+
+        Dim x As Integer
+        Dim y As Integer
+        Dim stOut As String
+
+        For y = 0 To 5
+            For x = 0 To 4
+                stOut = stOut & astPeople(x, y) & " "
+            Next
+            stOut = stOut & vbNewLine
+        Next
+
+        MsgBox(stOut)
+
+    End Sub
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+
+        Dim astPeople(4, 5) As String
+
+        astPeople(0, 0) = "Barrack"
+        astPeople(1, 0) = "Obama"
+        astPeople(2, 0) = "Male"
+        astPeople(3, 0) = "American"
+        astPeople(4, 0) = "President"
+
+        astPeople(0, 1) = "Jacinda"
+        astPeople(1, 1) = "Ardern"
+        astPeople(2, 1) = "Female"
+        astPeople(3, 1) = "New Zealand"
+        astPeople(4, 1) = "Prime Minister"
+
+        astPeople(0, 2) = "Ada"
+        astPeople(1, 2) = "Lovelace"
+        astPeople(2, 2) = "Female"
+        astPeople(3, 2) = "British"
+        astPeople(4, 2) = "Mathematician"
+
+        astPeople(0, 3) = "Albert"
+        astPeople(1, 3) = "Einstein"
+        astPeople(2, 3) = "Male"
+        astPeople(3, 3) = "Swiss"
+        astPeople(4, 3) = "Scientist"
+
+        astPeople(0, 4) = "Mahatama"
+        astPeople(1, 4) = "Gandhi"
+        astPeople(2, 4) = "Male"
+        astPeople(3, 4) = "Indian"
+        astPeople(4, 4) = "Lawyer"
+
+        astPeople(0, 5) = "Vincent"
+        astPeople(1, 5) = "van Gogh"
+        astPeople(2, 5) = "Male"
+        astPeople(3, 5) = "Dutch"
+        astPeople(4, 5) = "Artist"
+
+        Dim x As Integer
+        Dim y As Integer
+        Dim bFound As Boolean
+        Dim stTarget As String
+        Dim stOut As String
+
+        bFound = False
+        stTarget = InputBox("Enter the surname of the person you want to find")
+
+        For y = 0 To 5
+            If UCase(astPeople(1, y)) = UCase(stTarget) Then
+                bFound = True
+                Exit For
+            End If
+        Next
+
+        If bFound = True Then
+            For x = 0 To 4
+                stOut = stOut & astPeople(x, y) & " "
+            Next
+        Else
+            stOut = "Not Found"
+        End If
+
+        MsgBox(stOut)
+
+    End Sub
 End Class
